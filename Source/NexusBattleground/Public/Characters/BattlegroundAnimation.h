32ae8f82e264857c2927ba2f90735d2026da0853
Bijay-Desktop <bjsoft1@gmail.com>
2025-08-23 17:51:45 +0545

    adding weapon and charactr animations

--- Source/NexusBattleground/Public/Characters/BattlegroundAnimation.h
+++ Source/NexusBattleground/Public/Characters/BattlegroundAnimation.h
@@ -1,14 +1,22 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
-
 #include "CoreMinimal.h"
 #include "Animation/AnimInstance.h"
 #include "BattlegroundAnimation.generated.h"
 
-/**
- * 
- */
+UENUM(BlueprintType)
+enum class EAnimationOffsets : uint8
+{
+	AO_None UMETA(DisplayName = "None"),
+	AO_NormalStand UMETA(DisplayName = "Normal Stand"),
+	AO_NormalCrouch UMETA(DisplayName = "Normal Crouch"),
+	AO_RifleStand UMETA(DisplayName = "Rifle Stand"),
+	AO_RifleCrouch UMETA(DisplayName = "Rifle Crouch"),
+	AO_RifleStandAim UMETA(DisplayName = "Rifle Stand Aim"),
+	AO_RifleCrouchAim UMETA(DisplayName = "Rifle Crouch Aim"),
+};
+
 UCLASS()
 class NEXUSBATTLEGROUND_API UBattlegroundAnimation : public UAnimInstance
 {
